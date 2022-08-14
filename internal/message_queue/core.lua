7532f5ceb62950b844cdb2874e8cc52a06c6ac33
Rianov Viacheslav <slavarianov@yandex.ru>
2022-08-15 02:28:27 +0300

    refactored message queue

--- internal/message_queue/core.lua
+++ internal/message_queue/core.lua
@@ -11,19 +11,15 @@ box.schema.space.create('msg_queue',
             format = { { 'chat_id', type = 'uuid' },
                        { 'msg_id', type = 'uuid' , unique = true },
                        { 'sender_id', type = 'uuid' },
-                       { 'receiver_id', type = 'uuid' },
                        { 'payload', type = 'string' },
                        { 'created_at', type = 'number' } }
         })
 
-box.space.msg_queue:create_index('chat_index', {if_not_exists = true, parts = { { 1, 'uuid' }, {6, 'number'} }})
+box.space.msg_queue:create_index('chat_index', {if_not_exists = true, parts = { { 1, 'uuid' }, {5, 'number'} }})
 
 box.space.msg_queue:create_index('msg_index', {if_not_exists = true, parts = { { 2, 'uuid' }}})
 
 box.space.msg_queue:create_index('sender_index', {if_not_exists = true, unique=false, parts = { { 3, 'uuid' }, {1, 'uuid'}}})
-
-box.space.msg_queue:create_index('receiver_index', {if_not_exists = true,unique=false, parts = { { 4, 'uuid' }, {1, 'uuid'}}})
-
 
 -- This space is needed to keep the info about what for chats have any user
 -- and what users take part in chat.
@@ -32,22 +28,23 @@ box.schema.space.create('user_chat_list',
             if_not_exists = true,
             format = { { 'participant', type = 'uuid' },
                        { 'chat_id' , type = 'uuid'},
-                       { 'created_at', type = 'number' }} -- last message stamp?
+                       { 'last_read', type = 'number' }} -- last message stamp? last_read field?
         })
 
-box.space.user_chat_list:create_index('chat_list_index', { if_not_exists = true, unique =true, parts = { { 1, 'uuid' }, { 2, 'uuid' }} })
-box.space.user_chat_list:create_index('chat_list_index_inverted', { if_not_exists = true, unique =true, parts = { { 2, 'uuid' }, { 1, 'uuid' }} })
+box.space.user_chat_list:create_index('participant_index', { if_not_exists = true, unique =true, parts = { { 1, 'uuid' }, { 2, 'uuid' }} })
+box.space.user_chat_list:create_index('chat_id_index', { if_not_exists = true, unique =true, parts = { { 2, 'uuid' }, { 1, 'uuid' }} })
+
+box.schema.space.create('chats_upd',
+        {
+            if_not_exists = true,
+            format = { { 'chat_id', type = 'uuid', unique = true  },
+                       { 'sender_id', type = 'uuid' },
+                       { 'payload', type = 'string' },
+                       { 'created_at', type = 'number' } }
+        })
+box.space.chats_upd:create_index('chat_id_index', { if_not_exists = true, unique =true, parts = { { 1, 'uuid' }} })
 
 local queue = {}
-
--- in this table we keep chat_ids and receiver_ids with the value of last unread message
-local chats = {}
-
--- in this table we keep chat updates for receiver_id
-local chats_upd = {}
-
--- sync thing
-queue._wait = fiber.channel()
 
 --chat_id, sender_id, receiver_id, payload
 --c5f0ae14-d06b-4ffd-9bcd-6df01243a9c5, 62391bd9-157c-4513-8e7c-c082e00d2b7e, 61f98c94-de3c-491a-b9c7-1ea214b3ec13
@@ -59,40 +56,22 @@ function queue.put(chat_id, sender_id, receiver_id, payload)
     print(chat_id, sender_id, receiver_id, payload)
     print('put end')
 
-    if chats[chat_id] == nil then
-        chats[chat_id] = {}
-    end
-
-    if not chats[chat_id][receiver_id] or chats[chat_id][receiver_id] == -1 then
-        chats[chat_id][receiver_id] = created_at
-    end
-
     -- we put the id of the last message for chat list update
-    box.space.user_chat_list:replace{
+    box.space.chats_upd:replace{
+        uuid.fromstr(chat_id),
         uuid.fromstr(sender_id),
-        uuid.fromstr(chat_id),
+        payload,
         created_at,
     }
-    box.space.user_chat_list:replace{
-        uuid.fromstr(receiver_id),
-        uuid.fromstr(chat_id),
-        created_at,
-    }
-
-    if not chats_upd[chat_id] then
-        chats_upd[chat_id] = {}
-    end
-    chats_upd[chat_id] = msg_id:str()
 
     local res = box.space.msg_queue:insert{
         uuid.fromstr(chat_id),
         msg_id,
         uuid.fromstr(sender_id),
-        uuid.fromstr(receiver_id),
         payload, created_at}
 
     if res ~= nil then
-        return{chat_id, msg_id:str(), sender_id, receiver_id, payload, created_at}
+        return{chat_id, msg_id:str(), sender_id, payload, created_at}
     end
 
     return res
@@ -100,38 +79,54 @@ end
 
 
 function queue.take_new_messages_from_space(chat_id, receiver_id)
-    local since = 0
-
-    if not chats[chat_id] then
-        return nil
+    local chat_info = box.space.chats_upd.index.chat_id_index:get({ uuid.fromstr(chat_id) })
+    local last_updated = nil
+    if chat_info == nil then
+        box.space.user_chat_list:replace({uuid.fromstr(chat_id), uuid.fromstr(receiver_id), 0})
+    else
+        last_updated = chat_info[4]
     end
 
-    if chats[chat_id][receiver_id] == -1 then
-        return nil
+    local since_not_read = 0
+    local user_info = box.space.user_chat_list.index.participant_index:get({uuid.fromstr(receiver_id), uuid.fromstr(chat_id)})
+    if user_info == nil then
+        box.space.user_chat_list:replace({uuid.fromstr(receiver_id), uuid.fromstr(chat_id), 0})
+    else
+        since_not_read = user_info[3]
     end
-
-    since = chats[chat_id][receiver_id]
-    chats[chat_id][receiver_id] = -1
 
     local batch = {}
     for _, tuple in box.space.msg_queue.index.chat_index:pairs({ uuid.fromstr(chat_id) }) do
-        if (since ~= nil and since ~=-1 and since <= tuple[6]) then
-            table.insert(batch, { tuple[1]:str(), tuple[2]:str(), tuple[3]:str(), tuple[4]:str(), tuple[5], tuple[6] })
-            print(tuple[1]:str(), tuple[2]:str(), tuple[3]:str(), tuple[4]:str(), tuple[5], tuple[6])
+        if (since_not_read <= tuple[5]) then
+            table.insert(batch, { tuple[1]:str(), tuple[2]:str(), tuple[3]:str(), tuple[4], tuple[5] })
+            --print(tuple[1]:str(), tuple[2]:str(), tuple[3]:str(), tuple[4], tuple[5])
         end
     end
+
+    box.space.user_chat_list:replace({
+        uuid.fromstr(receiver_id),
+        uuid.fromstr(chat_id),
+        fiber.time()
+    })
     return batch
 end
 
 
-function queue.fetch_chat_list_update(chat_list)
+function queue.fetch_chat_list_update(user_id, chat_list)
     local batch = {}
     for _, chat_id in ipairs(chat_list) do
-        local msg_id = chats_upd[chat_id[2]:str()]
-        if msg_id ~= nil then
-            local tuple = box.space.msg_queue.index.msg_index:get(uuid.fromstr(msg_id))
-            table.insert(batch, { tuple[1]:str(), tuple[3]:str(), tuple[5], tuple[6] })
-            chats_upd[chat_id[2]:str()] = nil
+        if chat_id ~= nil and user_id ~= nil then
+            print('passed 1')
+            print(chat_id)
+            print(user_id)
+            local chat = box.space.chats_upd.index.chat_id_index:get({chat_id[2]})
+            print('passed 2')
+            local user_info = box.space.user_chat_list.index.participant_index:get({uuid.fromstr(user_id), chat_id[2]})
+            print('passed 3')
+            if chat[4] > user_info[3] then
+                table.insert(batch, { chat[1]:str(), chat[2]:str(), chat[3], chat[4] })
+            end
+            print('passed 4')
         end
     end
     return batch
@@ -139,7 +134,7 @@ end
 
 function queue.fetch_chat_list_update_for_single_user(user_id)
     local chat_list = box.space.user_chat_list:select(uuid.fromstr(user_id))
-    return queue.fetch_chat_list_update(chat_list)
+    return queue.fetch_chat_list_update(user_id, chat_list)
 end
 
 function queue.flush_all()
