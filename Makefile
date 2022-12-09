51cc00cc1e14bb95ad60410dd8d16977cd75f53a
Rianov Viacheslav <slavarianov@yandex.ru>
2022-12-10 00:35:37 +0300

    fixes for gateway

--- Makefile
+++ Makefile
@@ -5,11 +5,10 @@ run:
 	docker-compose up --remove-orphans --build
 
 migrate-users:
-	migrate -path ./internal/auth/schema -database 'postgres://postgres:admin@0.0.0.0:5433/users?sslmode=disable' up
+	migrate -path ./internal/user_data/db/migrations -database 'postgres://postgres:admin@0.0.0.0:5433/users?sslmode=disable' up
 
 migrate-users-drop:
-	migrate -path ./users-service/schema -database 'postgres://postgres:adminy@0.0.0.0:5433/users?sslmode=disable' drop
-
+	migrate -path ./internal/user_data/db/migrations -database 'postgres://postgres:adminy@0.0.0.0:5433/users?sslmode=disable' drop
 
 user-data-service-up:
 	docker-compose build && docker-compose up
--- internal/auth/cmd/main.go
+++ internal/auth/cmd/main.go
@@ -16,28 +16,9 @@ import (
 	"github.com/spf13/viper"
 )
 
-// @title Swagger Example API
-// @version 1.0
-// @description This is a sample server Petstore server.
-// @termsOfService http://swagger.io/terms/
-
-// @contact.name API Support
-// @contact.url http://www.swagger.io/support
-// @contact.email support@swagger.io
-
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host petstore.swagger.io
-// @BasePath /v2
-
 func main() {
-
-	fmt.Println("Starting..")
 	configPath := os.Getenv("AUTH_CONFIG")
-	p, _ := os.Getwd()
-	fmt.Println(p, configPath)
-	configPath = "configs"
+
 	viper.AddConfigPath(configPath)
 	viper.SetConfigName("auth-config.yaml")
 	viper.SetConfigType("yaml")
