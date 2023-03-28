b17b0766bd3bc5bbb7a7ed0ad16e4b9ecd4effc2
Rianov Viacheslav <slavarianov@yandex.ru>
2023-03-28 15:47:50 +0300

    add jwt generating

---
+++ internal/pkg/jwt.go
@@ -0,0 +1,47 @@
+package pkg
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"our-little-chatik/internal/models"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/golang/glog"
+)
+
+func GetSignedKey() (string, error) {
+	key := os.Getenv("JWT_SIGNED_KEY")
+	if key == "" {
+		return "", fmt.Errorf("no env provided")
+	}
+	return key, nil
+}
+
+func GenerateJWTToken(user models.User, expireCookie bool) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["authorized"] = true
+	claims["UserID"] = user.UserID.String()
+	if expireCookie {
+		claims["exp"] = time.Now().Add(-time.Hour * 30).Unix()
+	} else {
+		claims["exp"] = time.Now().Add(time.Hour * 30).Unix()
+	}
+
+	mySignedKey, err := GetSignedKey()
+	if err != nil {
+		return "", err
+	}
+
+	tokenString, err := token.SignedString([]byte(mySignedKey))
+	if err != nil {
+		glog.Error("generating JWT Token failed")
+		return "", err
+	}
+
+	return tokenString, nil
+}
