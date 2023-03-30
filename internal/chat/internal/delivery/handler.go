d5c618dd988af79d262cece611f8a5dae8cdefa6
Rianov Viacheslav <slavarianov@yandex.ru>
2023-03-30 20:20:14 +0300

    auth hook fix

--- internal/chat/internal/delivery/handler.go
+++ internal/chat/internal/delivery/handler.go
@@ -36,7 +36,13 @@ func NewChatHandler(usecase internal.ChatUseCase) *ChatHandler {
 // @Failure 500
 // @Router /chat/conv [get]
 func (c *ChatHandler) GetChatMessages(w http.ResponseWriter, r *http.Request) {
-	_, err := pkg.AuthHook(r)
+	var err error
+	defer func() {
+		if err != nil {
+			glog.Error(err)
+		}
+	}()
+	_, err = pkg.AuthHook(r)
 	if err != nil {
 		w.WriteHeader(http.StatusForbidden)
 		errObj := models.Error{Msg: "Invalid token"}
@@ -94,6 +100,11 @@ func (c *ChatHandler) GetChatMessages(w http.ResponseWriter, r *http.Request) {
 // @Router /accounts/{id} [get]
 func (clh *ChatHandler) GetChatList(w http.ResponseWriter, r *http.Request) {
 	var err error
+	defer func() {
+		if err != nil {
+			glog.Error(err)
+		}
+	}()
 	user, err := pkg.AuthHook(r)
 	if err != nil {
 		w.WriteHeader(http.StatusForbidden)
@@ -121,6 +132,12 @@ func (clh *ChatHandler) GetChatList(w http.ResponseWriter, r *http.Request) {
 }
 
 func (clh *ChatHandler) PostNewChat(w http.ResponseWriter, r *http.Request) {
+	var err error
+	defer func() {
+		if err != nil {
+			glog.Error(err)
+		}
+	}()
 	usr, err := pkg.AuthHook(r)
 	if err != nil {
 		w.WriteHeader(http.StatusForbidden)
@@ -135,6 +152,16 @@ func (clh *ChatHandler) PostNewChat(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
+	}
+
+	for _, participant := range chat.Participants {
+		if participant == uuid.Nil {
+			w.WriteHeader(http.StatusBadRequest)
+			errObj := models.Error{Msg: "Added unexisting participant"}
+			body, _ := json.Marshal(errObj)
+			w.Write(body)
+			return
+		}
 	}
 
 	chat.Participants = append(chat.Participants, usr.UserID)
@@ -154,6 +181,12 @@ func (clh *ChatHandler) PostNewChat(w http.ResponseWriter, r *http.Request) {
 }
 
 func (clh *ChatHandler) PostChat(w http.ResponseWriter, r *http.Request) {
+	var err error
+	defer func() {
+		if err != nil {
+			glog.Error(err)
+		}
+	}()
 	usr, err := pkg.AuthHook(r)
 	if err != nil {
 		w.WriteHeader(http.StatusForbidden)
