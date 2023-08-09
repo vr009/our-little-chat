3684daebcfac69c39133a9d9fcf980d345241576
vr009 <slavarianov@yandex.ru>
2023-08-09 12:23:23 +0800

    flusher fix

--- internal/models/chat_item.go
+++ internal/models/chat_item.go
@@ -1,15 +1,14 @@
 package models
 
 import (
+	"database/sql"
 	"github.com/google/uuid"
 )
 
 type ChatItem struct {
-	ChatID          uuid.UUID `json:"chat_id"`
-	Name            string    `json:"name,omitempty"`
-	PhotoURL        string    `json:"photo_url,omitempty"`
-	LastSender      uuid.UUID `json:"last_sender"`
-	MsgID           uuid.UUID `json:"msg_id"`
-	LastMsg         string    `json:"last_msg"`
-	LastMessageTime int64     `json:"last_message"`
+	ChatID          uuid.UUID      `json:"chat_id"`
+	Name            string         `json:"name,omitempty"`
+	PhotoURL        string         `json:"photo_url,omitempty"`
+	LastMsg         sql.NullString `json:"last_msg"`
+	LastMessageTime sql.NullInt64  `json:"last_message"`
 }
