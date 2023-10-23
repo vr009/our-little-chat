f0f0c222b65928f4198fb618195f37ec4aed98eb
vr009 <slavarianov@yandex.ru>
2023-10-24 00:53:11 +0400

    peer: add structured messages when connecting to service

--- internal/peer/internal/models/notification.go
+++ internal/peer/internal/models/notification.go
@@ -1,7 +1,5 @@
 package models
 
-import "our-little-chatik/internal/models"
-
 type NotificationType string
 
 const (
@@ -9,8 +7,24 @@ const (
 	ChatMessage NotificationType = "chat"
 )
 
+// Notification is a type that gets encoded into a json document when communicating
+// with peer service. Type field describes whether the notification has service info
+// purpose or it is a message. Body may contain models.Message or other service structures.
 type Notification struct {
-	Type        NotificationType `json:"type,omitempty"`
-	Message     *models.Message  `json:"message,omitempty"`
-	Description string           `json:"description,omitempty"`
+	Type NotificationType `json:"type,omitempty"`
+	Body interface{}      `json:"body,omitempty"`
+}
+
+type ConnectionStatusType string
+
+const (
+	Established ConnectionStatusType = "established"
+	Failed      ConnectionStatusType = "failed"
+	Conflict    ConnectionStatusType = "conflict"
+)
+
+// PeerConnectionStatus is a type for notifying peer about a connection status.
+type PeerConnectionStatus struct {
+	Status     ConnectionStatusType
+	Properties map[string]any
 }
